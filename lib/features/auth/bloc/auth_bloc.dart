902d630a99c256a6808daa67af79953117587c47
dvlpr-aadeshsahane <dev.aadeshsahane@gmail.com>
2025-08-21 15:46:15 +0530

    Revert "Revert "Authentication completed""

---
+++ lib/features/auth/bloc/auth_bloc.dart
@@ -0,0 +1,73 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:turfbooking/features/auth/bloc/auth_state.dart';
+import 'auth_event.dart';
+
+import '../repository/auth_repository.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository authRepository;
+
+  AuthBloc(this.authRepository) : super(AuthInitial()) {
+    on<AuthLoginRequested>(_onLoginRequested);
+    on<AuthSignupRequested>(_onSignupRequested);
+    on<AuthLogoutRequested>(_onLogoutRequested);
+
+    authRepository.userChanges.listen((user) {
+      add(AuthUserChanged(user));
+    });
+
+    on<AuthUserChanged>((event, emit) {
+      if (event.user != null) {
+        emit(AuthAuthenticated(event.user as dynamic));
+      } else {
+        emit(AuthUnauthenticated());
+      }
+    });
+  }
+
+  Future<void> _onLoginRequested(
+    AuthLoginRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      final user = await authRepository.login(event.email, event.password);
+      if (user != null) {
+        emit(AuthAuthenticated(user));
+      } else {
+        emit(AuthUnauthenticated());
+      }
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
+
+  Future<void> _onSignupRequested(
+    AuthSignupRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      final user = await authRepository.signupUser(
+        email: event.email,
+        name: event.name,
+        password: event.password,
+      );
+      if (user != null) {
+        emit(AuthAuthenticated(user));
+      } else {
+        emit(AuthUnauthenticated());
+      }
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
+
+  Future<void> _onLogoutRequested(
+    AuthLogoutRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    await authRepository.logout();
+    emit(AuthUnauthenticated());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:turfbooking/core/router.dart';
+import 'package:turfbooking/features/auth/bloc/auth_bloc.dart';
+import 'package:turfbooking/features/auth/repository/auth_repository.dart';
 import 'package:turfbooking/firebase_options.dart';
 
 void main() async {
@@ -17,7 +20,15 @@ class TurfBooking extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: Size(375, 814),
-      child: MaterialApp.router(routerConfig: goRouter),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => AuthBloc(AuthRepository())),
+        ],
+        child: MaterialApp.router(
+          routerConfig: goRouter,
+          debugShowCheckedModeBanner: false,
+        ),
+      ),
     );
   }
 }
